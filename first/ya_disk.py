67b53fe0dcc5488087438abf45161702371bee36
Your Name <kurskovp@gmail.com>
2021-03-11 23:33:58 +0300

    Ввели номер ТОКЕНА и параметры ввода токена через heanders

--- first/ya_disk.py
+++ first/ya_disk.py
@@ -1,7 +1,11 @@
 import requests
 
+TOKEN = 'AgAAAA16yaTAADLWBlKRwrYhUimmUWQ4zu39e4'
 
-respons = requests.get('https://cloud-api.yandex.net/v1/disk/resources/upload', params={'path':'upload_file.txt'})
+respons = requests.get('https://cloud-api.yandex.net/v1/disk/resources/upload',
+                       params={'path':'upload_file.txt'},
+                       heanders = {'Authorizatijn': fr'OAuth {TOKEN}'})
+
 
 print(respons.status_code)
 print(respons.json())
