e088dd620636b6d41ba7cdb85578e9336d7e0111
Your Name <kurskovp@gmail.com>
2021-03-11 23:16:09 +0300

    Сформировали запрос с применением параметра parms с создание славаря в конце url

--- first/ya_disk.py
+++ first/ya_disk.py
@@ -1,4 +1,4 @@
 import requests
 
 
-respons = requests.get('https://cloud-api.yandex.net/v1/disk/resources/upload?path=upload_file.txt')
+respons = requests.get('https://cloud-api.yandex.net/v1/disk/resources/upload', params={'path':'upload_file.txt'})
