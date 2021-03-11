1574cdf83d1956ff47bd7b89443a101dbe96571a
Your Name <kurskovp@gmail.com>
2021-03-11 23:19:37 +0300

    Записываем первые проверочные принты, при которых Яндекс Диск даёт ответ об отсутствии Торента!

--- first/ya_disk.py
+++ first/ya_disk.py
@@ -2,3 +2,6 @@ import requests
 
 
 respons = requests.get('https://cloud-api.yandex.net/v1/disk/resources/upload', params={'path':'upload_file.txt'})
+
+print(respons.status_code)
+print(respons.json())
