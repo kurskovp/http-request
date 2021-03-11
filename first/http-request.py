54d16021fb0565b39b88a9e72d621e1ed3b6e6fe
Your Name <kurskovp@gmail.com>
2021-03-11 14:44:00 +0300

    Добавлно git_name: которая будет присваивать имени файла хранения гиф из -1 части url копироваемого файла

--- first/http-request.py
+++ first/http-request.py
@@ -14,6 +14,7 @@ for post in posts:
     url = post['data']['url']
     if 'imgur' not in url:
         continue
+    git_name = url.split('/')[-1]
     with open(gif_name, mode='wb') as f:
         data_gif = requests.get(url)
         f.write(data_gif.content)
