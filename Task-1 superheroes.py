b2888220d6edc8f6108cd18da8fde9f3045e8dca
Your Name <kurskovp@gmail.com>
2021-03-15 15:04:53 +0300

    Создаем сравнение по паказателям интелекта и выводим на экран супергероя с лушим интелектом

--- Task-1 superheroes.py
+++ Task-1 superheroes.py
@@ -17,6 +17,15 @@ intell_1 = data_1['results'][0]['powerstats']['intelligence']
 intell_2 = data_2['results'][0]['powerstats']['intelligence']
 intell_3 = data_3['results'][0]['powerstats']['intelligence']
 
-print(name_1, intell_1)
-print(name_2, intell_2)
-print(name_3, intell_3)
+# print(name_1, intell_1)
+# print(name_2, intell_2)
+# print(name_3, intell_3)
+
+a, b, c = (name_1, int(intell_1)), (name_2, int(intell_2)), (name_3, int(intell_3))
+mx = a
+if b >mx:
+    mx = b
+if c > mx:
+    mx = c
+
+print('Наивысший интелект имеет: ', mx)
