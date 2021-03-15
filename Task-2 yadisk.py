0ca656dd92480b27553adb075eadd624522874b6
Your Name <kurskovp@gmail.com>
2021-03-15 17:56:48 +0300

    Выводит ошибку Traceback (most recent call last):   File "/home/kurskovp/d/PYTHON/PycharmProjects/dz_9/Task-2 yadisk.py", line 22, in <module>     with open('0FoqeSA.gif', 'rb') as f: FileNotFoundError: [Errno 2] No such file or directory: '0FoqeSA.gif' 200 {'operation_id': '049f59d4b91ab18f70d9b639aedff43fa46c934c26b58a17f95098e8fb5f50be', 'href': 'https://uploader7o.disk.yandex.net:443/upload-target/20210315T175433.517.utd.c84gnl12bfawnd03bdfp5b9jk-k7o.3448551', 'method': 'PUT', 'templated': False}
    Process finished with exit code 1

--- Task-2 yadisk.py
+++ Task-2 yadisk.py
@@ -3,13 +3,22 @@ import requests
 TOKEN = input()
 
 response = requests.put('https://cloud-api.yandex.net/v1/disk/resources',
-                                params={'path': 'kurskovp'},
-                                headers={'Authorization': f'OAuth {TOKEN}'})
-
-response_2 = requests.get('https://cloud-api.yandex.net/v1/disk/resources',
                         params={'path': 'kurskovp'},
                         headers={'Authorization': f'OAuth {TOKEN}'})
 
+response_2 = requests.get('https://cloud-api.yandex.net/v1/disk/resources/upload',
+                        params={'path': '0FoqeSA.gif&url=https%3A%2F%2Fcloud-api.yandex.net%2Fv1%2Fdisk%2Fresources%3Fpath%3Dkurskovp'},
+                        headers={'Authorization': f'OAuth {TOKEN}'})
+#
+# response_3 = requests.get('https://cloud-api.yandex.net/v1/disk/resources/upload',
+#                         params={'path': '0FoqeSA.gif&url=https%3A%2F%2Fcloud-api.yandex.net'
+#                                         '%2Fv1%2Fdisk%2Fresources%3Fpath%3Dkurskovp'},
+#                         headers={'Authorization': f'OAuth {TOKEN}'})
+
+
 print(response_2.status_code)
 print(response_2.json())
+href = response_2.json()['href']
+with open('0FoqeSA.gif', 'rb') as f:
+    requests.put(href, files = {'file': f})
 
