3491e72d3302e2be1d46463ddb3a1cb68c19ba0a
Your Name <kurskovp@gmail.com>
2021-03-15 14:44:38 +0300

    Создаем запрросы по сай с характеристиками супергероев и проверяем работоспособность запросов

--- Task-1 superheroes.py
+++ Task-1 superheroes.py
@@ -1,2 +1,9 @@
 import requests
 
+response_1 = requests.get('https://superheroapi.com/api/2619421814940190/search/Hulk')
+response_2 = requests.get('https://superheroapi.com/api/2619421814940190/search/Captain America')
+response_3 = requests.get('https://superheroapi.com/api/2619421814940190/search/Thanos')
+
+print(response_1.status_code)
+print(response_2.status_code)
+print(response_3.status_code)
